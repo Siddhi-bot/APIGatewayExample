b5e04441fd8b14bafa9ed9ebedb57b20559be99e
Siddhi <67221087+Siddhi-bot@users.noreply.github.com>
2021-07-30 13:45:50 +0530

    In this API,Spring-Cloud-Circuit-Breaker is made

---
+++ Spring-Cloud-Circuit-Breaker-Tutorial-master/Circuit-Breaker/src/test/java/com/siddhi/demo/CircuitBreakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddhi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Spring-Cloud-Circuit-Breaker-Tutorial-master/Hello-Service/src/test/java/com/siddhi/demo/HelloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddhi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
